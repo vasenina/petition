924a68b58b5906ee7957afb2a1e3b40974116b91
Vasenina Yuliya <mamicheva@gmail.com>
2021-12-10 18:01:20 +0100

    scookies

--- server.js
+++ server.js
@@ -26,9 +26,15 @@ app.use(
 app.get("/", (req, res) => {
     //console.log("req.session before: ", req.session);
     req.session.onion = "bigSecret99";
-    console.log("req.session before: ", req.session);
+    // console.log("req.session before: ", req.session);
 
     res.redirect("/petition");
+});
+
+app.get("/clear", (req, res) => {
+    req.session = null;
+    console.log("Clear cookies", req.session);
+    res.redirect("/");
 });
 
 app.get("/petition", (req, res) => {
@@ -36,6 +42,11 @@ app.get("/petition", (req, res) => {
     console.log(signedError);
     // console.log(req.query);
     if (req.session.onion === "bigSecret99") {
+        if (req.session.id) {
+            console.log("I know this user:", req.session.id);
+            res.redirect("/thanks");
+            return;
+        }
         res.render("petition", {
             layout: "main",
             signedError,
@@ -47,49 +58,57 @@ app.get("/petition", (req, res) => {
 });
 
 app.get("/signers", (req, res) => {
-    db.getSigners()
-        .then(({ rows }) => {
-            //console.log("results.rows", rows);
-            res.render("signers", {
-                layout: "main",
-                signers: rows,
+    if (req.session?.id) {
+        db.getSigners()
+            .then(({ rows }) => {
+                //console.log("results.rows", rows);
+                res.render("signers", {
+                    layout: "main",
+                    signers: rows,
+                });
+            })
+            .catch((err) => {
+                console.log("err in getSigners", err);
             });
-        })
-        .catch((err) => {
-            console.log("err in getSigners", err);
-        });
+    } else {
+        res.redirect("/petition");
+    }
 });
 
 app.get("/thanks", (req, res) => {
-    const countPromise = db.getCountOfSigners();
-    const signaturePromise = db.getSignature(req.session.id);
-    Promise.all([countPromise, signaturePromise])
-        .then((value) => {
-            // console.log(signersCount.rows[0].count);
-            // console.log(
-            //     "there is a signature from data",
-            //     value[1].rows[0].signimg
-            // );
-            res.render("thanks", {
-                layout: "main",
-                signersCount: value[0].rows[0].count,
-                signature: value[1].rows[0].signimg,
+    if (req.session?.id) {
+        const countPromise = db.getCountOfSigners();
+        const signaturePromise = db.getSignature(req.session.id);
+        Promise.all([countPromise, signaturePromise])
+            .then((value) => {
+                // console.log(signersCount.rows[0].count);
+                // console.log(
+                //     "there is a signature from data",
+                //     value[1].rows[0].signimg
+                // );
+                res.render("thanks", {
+                    layout: "main",
+                    signersCount: value[0].rows[0].count,
+                    signature: value[1].rows[0].signimg,
+                });
+            })
+            .catch((err) => {
+                console.log("Error in Thanks Promises", err);
+                res.render("thanks", {
+                    layout: "main",
+                });
             });
-        })
-        .catch((err) => {
-            console.log("Error in Thanks Promises", err);
-            res.render("thanks", {
-                layout: "main",
-            });
-        });
+    } else {
+        res.redirect("/petition");
+    }
 });
 
 app.post("/petition", (req, res) => {
     console.log("im in POST petition");
-    //console.log("SIGNATURE:", req.body);
+
     db.addSign(req.body.first, req.body.last, req.body.signature)
         .then(({ rows }) => {
-            console.log("ID new user:", rows[0].id);
+            // console.log("ID new user:", rows[0].id);
             req.session.id = rows[0].id;
             res.redirect("/thanks");
         })
