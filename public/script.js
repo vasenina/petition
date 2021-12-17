8c1621a99d40f5cc6cfff16a67f90326b861c1e2
Vasenina Yuliya <mamicheva@gmail.com>
2021-12-17 11:58:11 +0100

    touch signature

--- public/script.js
+++ public/script.js
@@ -5,36 +5,51 @@ console.log("i'm from script.js");
     var ctx = can.getContext("2d");
     let mousedown = false;
 
-    //  ["mousedown", "touchstart"].forEach((ev) => {
-    can.addEventListener("mousedown", (event) => {
-        mousedown = true;
-        console.log("mousedown");
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "#f21d81";
-        ctx.moveTo(event.offsetX, event.offsetY);
+    ["mousedown", "touchstart"].forEach((type) => {
+        can.addEventListener(type, (event) => {
+            mousedown = true;
+            console.log("mousedown");
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "#f21d81";
+            const point = getXY(type, event, ["mousedown", "touchstart"]);
+            ctx.moveTo(point.x, point.y);
+            event.preventDefault();
+        });
     });
-    // });
 
-    // ["mousemove", "touchmove"].forEach((ev) => {
-    can.addEventListener("mousemove", (event) => {
-        if (mousedown) {
-            console.log("mousemove", event);
-            ctx.lineTo(event.offsetX, event.offsetY);
-            ctx.stroke();
+    ["mousemove", "touchmove"].forEach((type) => {
+        can.addEventListener(type, (event) => {
+            if (mousedown) {
+                const point = getXY(type, event, ["mousemove", "touchmove"]);
+                ctx.lineTo(point.x, point.y);
+                ctx.stroke();
+                event.preventDefault();
+            }
+        });
+    });
+
+    function getXY(type, event, types) {
+        let x = 0,
+            y = 0;
+        if (type == types[0]) {
+            x = event.offsetX;
+            y = event.offsetY;
+        } else if (type == types[1]) {
+            const touches = event.changedTouches;
+            x = touches[0].pageX;
+            y = touches[0].pageY - can.offsetTop;
         }
-    });
-    // });
+        return { x, y };
+    }
 
-    // ["mouseup", "touchend"].forEach((ev) => {
-    can.addEventListener("mouseup", (event) => {
-        mousedown = false;
-        //const sign = can.toDataURL("image/jpeg", 1);
-        const signImg = can.toDataURL("image/png");
+    ["mouseup", "touchend"].forEach((type) => {
+        can.addEventListener(type, (event) => {
+            mousedown = false;
+            const signImg = can.toDataURL("image/png");
 
-        $("#signInput").val(signImg);
-        // console.log("CANVAS:", can.toDataURL("image/jpeg", 0.3));
-        //console.log("Signature in input:", $("#signInput").val());
+            $("#signInput").val(signImg);
+            //event.preventDefault();
+        });
     });
-    // });
 })();
